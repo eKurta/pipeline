880cc5a5d5969949f26c22301a147df994a81307
eKurta <64108361+eKurta@users.noreply.github.com>
2021-11-27 15:34:32 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/pipeline/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pipeline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+   
+        primarySwatch: Colors.blue,
+      ),
+      home: PocetnaStrana(),
+    );
+  }
+}
+
+class PocetnaStrana extends StatelessWidget {
+  const PocetnaStrana({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(child:Text('Neki text'))
+      )
+    );
+  }
+}
